03531c86ec22664c870c68ae8903335875c7212d
alex <camward@yandex.ru>
2019-12-06 09:38:14 +0400

    настройка redux npm i redux react-redux

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { AppLoading } from 'expo'
+import { Provider } from 'react-redux'
 import { AppNavigation } from './src/navigation/AppNavigation'
 import { bootstrap } from './src/bootstrap'
+import store from './src/store'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false)
@@ -17,5 +19,9 @@ export default function App() {
     )
   }
 
-  return <AppNavigation />
+  return (
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
+  )
 }
